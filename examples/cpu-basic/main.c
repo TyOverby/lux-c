e35109a41088576826fa5963344ef381f95b632b
Ty Overby <ty@pre-alpha.com>
2026-03-18 15:17:28 -0400

    fixup cpu-basic example

--- examples/cpu-basic/main.c
+++ examples/cpu-basic/main.c
@@ -8,6 +8,6 @@ int main(void) {
   lux_instruction_buffer* buffer = lux_get_instruction_buffer(scene);
   lux_push_pixel(buffer, 0, 0, (lux_color){.r = 255, .g = 0, .b = 0, .a = 255});
   lux_color* output = malloc(256 * 256 * sizeof(lux_color));
-  lux_dispatch(scene, (lux_dispatch_args){.x = 0, .y = 0, .width = 256, .height = 256}, output);
+  lux_dispatch(scene, (lux_dispatch_args){.dx = 0, .dy = 0, .width = 256, .height = 256}, output);
   stbi_write_png("output.png", 256, 256, 4, output, 4);
 }
